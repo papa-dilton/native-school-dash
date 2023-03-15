e89e577a07c7760a46f61146343c2917c8f466a1
Dalton Harrold <76129078+papa-dilton@users.noreply.github.com>
2023-03-15 16:46:31 -0500

    Initial Commit

---
+++ native-school-dash/native_school_dashApp.swift
@@ -0,0 +1,17 @@
+//
+//  native_school_dashApp.swift
+//  native-school-dash
+//
+//  Created by Dalton on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct native_school_dashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
