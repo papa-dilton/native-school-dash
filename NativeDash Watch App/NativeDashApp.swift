7058a768f3e5f72f518ae07dea2e9b63754e9fe1
Dalton Harrold <djharrold1@gmail.com>
2023-09-13 11:08:50 -0500

    Initial Commit

---
+++ NativeDash Watch App/NativeDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeDashApp.swift
+//  NativeDash Watch App
+//
+//  Created by student on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct NativeDash_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NativeDash/NativeDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeDashApp.swift
+//  NativeDash
+//
+//  Created by student on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct NativeDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
