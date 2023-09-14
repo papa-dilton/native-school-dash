ccad7ce8d6c1341013808d3ad2a9a32f053e953d
Dalton Harrold <djharrold1@gmail.com>
2023-09-14 09:12:54 -0500

    initial circle paint

--- NativeDash/ContentView.swift
+++ NativeDash/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
-struct ContentView: View {
+extension Color {
+    public static var emptyRingColor: Color {
+        return Color(red: 0.8588235294117647, green: 0.9098039215686274, blue: 0.9372549019607843)
+    }
+
+    public static var fillColor: Color {
+        return Color(red: 0.30980392156862746, green: 0.5568627450980392, blue: 0.6980392156862745)
+    }
+}
+
+struct PeriodTimerRing: View {
+    @Binding var progress: CGFloat
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Circle()
+                .stroke(Color.emptyRingColor, lineWidth: 20)
+            Circle()
+                .rotation(Angle(degrees:(-(360*progress)-90)))
+                .trim(from: 0, to: progress)
+                .stroke(
+                    Color.fillColor,
+                    style: StrokeStyle(lineWidth: 20, lineCap: .round)
+            )
+        }.frame(idealWidth: 300, idealHeight: 300, alignment: .center)
+    }
+}
+
+struct ContentView: View {
+    @State private var progress: CGFloat = 0.2
+    var body: some View {
+        ZStack {
+            PeriodTimerRing(progress: $progress)
+                .fixedSize()
         }
-        .padding()
     }
 }
 
