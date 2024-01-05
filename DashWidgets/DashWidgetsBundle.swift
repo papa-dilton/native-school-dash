b621881db6f8ab519fefca0fa1ebf2fe44034f3b
Dalton Harrold <djharrold1@gmail.com>
2024-01-04 20:23:13 -0600

    add widget boilerplate

---
+++ DashWidgets/DashWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DashWidgetsBundle.swift
+//  DashWidgets
+//
+//  Created by Dalton Harrold on 1/4/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DashWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        DashWidgets()
+    }
+}
